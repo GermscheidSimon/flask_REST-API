6b7f0b15e273ece308e43dd54abe36dc6a624f03
Khaildoran <sgermscheid@gmail.com>
2020-12-02 16:15:27 -0600

    get request working

--- hello.py
+++ hello.py
@@ -1,12 +1,45 @@
-import datetime
-import os
 
-from flask import Flask
+
+from flask import Flask, jsonify
+import psycopg2
+import json
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hello World!"
+
+
+@app.route("/", methods=['GET'])
+def getHello():
+    print('GET REQ')
+    # Connect to your postgres DB
+    conn = psycopg2.connect("dbname=bookstore")
+
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+
+    # Execute a query
+    query = "SELECT * FROM library;"
+    cur.execute(query)
+
+    # Retrieve query results
+    records = cur.fetchall()
+    print(records)
+    return jsonify(records)
+
+@app.route("/", methods=['POST'])
+def postInfo():
+    print('POST REQ')
+    return 'post success'
+
+@app.route("/", methods=['PUT'])
+def putInfo():
+    print('PUT REQ')
+    return 'post success'
+
+@app.route("/", methods=['DELETE'])
+def deleteReq():
+    print('delete request')
+    return 'it\'s gone'
 
 if __name__ == "__main__":
   app.run()
