f1d19b52bc6feb3db022926d74e1c28e5ee0321a
Khaildoran <sgermscheid@gmail.com>
2020-12-02 18:02:12 -0600

    able to send JSON

--- hello.py
+++ hello.py
@@ -1,16 +1,19 @@
-
-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import psycopg2
+import calendar
+
 import json
 
 app = Flask(__name__)
 
 
-
 @app.route("/", methods=['GET'])
 def getHello():
     print('GET REQ')
+
+    cal = calendar.month(2020, 12)
+    print ("Here is the calendar:")
+    print (cal)
     # Connect to your postgres DB
     conn = psycopg2.connect("dbname=bookstore")
 
@@ -22,14 +25,29 @@ def getHello():
     cur.execute(query)
 
     # Retrieve query results
-    records = cur.fetchall()
+    records =   cur.fetchall()
     print(records)
     return jsonify(records)
 
 @app.route("/", methods=['POST'])
 def postInfo():
     print('POST REQ')
-    return 'post success'
+    print(request.is_json)
+    req = request.get_json()
+    print(req)
+    print(type(req))
+    # insertData = ['test','testing','today']
+    # conn = psycopg2.connect("dbname=bookstore")
+
+    # # Open a cursor to perform database operations
+    # cur = conn.cursor()
+    # # Execute a query
+    # query = "INSERT INTO library (title, author, published) VALUES (%s, %s, %s)"
+    # cur.execute(query, insertData)
+
+    # # Retrieve query results
+    # records =   cur.fetchall()
+    return 'ldkfjsljdfljs'
 
 @app.route("/", methods=['PUT'])
 def putInfo():
